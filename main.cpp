9d8a8b046caa7543231eb11358f16cb05e57bba6
E1E120017mutrafin <114495137+E1E120017mutrafin@users.noreply.github.com>
2022-09-27 10:08:01 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,82 @@
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-#include <GL/glut.h>
+#include <GL\glut.h>
+#include <math.h>
 
-void mydisplay(){
-    glClear(GL_COLOR_BUFFER_BIT);
+void display(void)
+{
 
- glBegin(GL_POLYGON);
-  glColor3f(0, 1, 1);
-  glVertex2f(-0.5, -0.5);
-  glVertex2f(-0.75, 0);
-  glVertex2f(-0.5, 0.5);
-  glVertex2f(0, 0.75);
-  glVertex2f(0.5, 0.5);
-  glVertex2f(0.75, 0);
-  glVertex2f(0.5, -0.5);
-  glVertex2f(0,-0.75);
- glEnd();
- glFlush();
+glClearColor(1.0,1.0,1.0,0.0);
+
+glMatrixMode(GL_PROJECTION);
+gluOrtho2D(0.0,300.0,0.0,300.0);
 }
-int main(int argc, char** argv){
- glutCreateWindow("Polygon Segi Delapan coded by KokoySitiKomariah");
- glutDisplayFunc(mydisplay);
- glutMainLoop();
+void setPixel (GLint xCoordinate, GLint yCoordinate)
+{
+glBegin (GL_POINTS);
+glVertex2i (xCoordinate, yCoordinate);
+glEnd();
+glFlush();
+}
+
+void lineBres (GLint x0, GLint y0, GLint xEnd, GLint yEnd)
+{
+GLint dx = (float) fabs ((float) xEnd - x0);
+GLint dy = (float) fabs ((float) yEnd - y0);
+GLint p = 2*dy-dx;
+GLint twoDy = 2*dy;
+GLint twoDyMinusDx = 2*(dy-dx);
+GLint x,y;
+
+if(x0 > xEnd){
+x=xEnd;
+y=yEnd;
+xEnd=x;
+}
+else{
+x=x0;
+y=y0;
+}
+setPixel(x,y);
+while(x<xEnd){
+x++;
+if(p<0)
+p+=twoDy;
+else{
+y++;
+p+=twoDyMinusDx;
+}
+setPixel(x,y);
+}
+}
+void drawMyLine (void)
+{
+    glClear (GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0,0.0,0.0);
+    glPointSize(4.0);
+    GLint x0 = 50;
+    GLint y0 = 50;
+    GLint xEnd = 150;
+    GLint yEnd = 50;
+    lineBres(x0,y0,xEnd,yEnd);
+
+
+}
+
+int main(int argc, char** argv)
+{
+
+glutInit(&argc,argv);
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+glutInitWindowSize(400,400);
+glutInitWindowPosition(0,0);
+glutCreateWindow("Digital Differential Analyzer Algorithm");
+display();
+glutDisplayFunc(drawMyLine);
+glutMainLoop();
+return 0;
 }
